c8bf6b48592d7a7ccb2f0b4965312264c7aee381
kadoppe <kadoppe@me.com>
2016-03-06 10:17:59 +0900

    Styleを整えました。

--- src/index.php
+++ src/index.php
@@ -1,4 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="/assets/application.css">
+</head>
+<body>
+<div id="catch">
 <?php
 require('lib/speaker.php');
 echo Speaker::sayHelloWorld();
-
+?>
+</div>
+</body>
+</html>
