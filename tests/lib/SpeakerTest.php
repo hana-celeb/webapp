dfae6e27bf1e8ea5732bf7cd3ca3d1daba621dec
kadoppe <kadoppe@me.com>
2016-03-06 13:42:48 +0900

    Hello Docker

--- tests/lib/SpeakerTest.php
+++ tests/lib/SpeakerTest.php
@@ -5,6 +5,6 @@ class SpeakerTest extends PHPUnit_Framework_TestCase
 {
   public function testCanSayHelloWorld()
   {
-    $this->assertEquals('Hello World!', Speaker::sayHelloWorld());
+    $this->assertEquals('Hello Docker!', Speaker::sayHelloWorld());
   }
 }
